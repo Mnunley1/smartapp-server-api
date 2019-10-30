b95756964e23f3d14f7919bfe926d40a60d78c64
Malcolm Nunley <Mxnunley1@gmail.com>
2019-10-30 07:28:25 -0400

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "postgresql-adjacent-50911",
-    user: "",
-    password: "Wfukidd92",
-    database: "smartapp"
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
